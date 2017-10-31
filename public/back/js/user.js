54299ef1121dbf332d03d28458147f52a0e50624
wh <747287473@qq.com>
2017-10-31 18:10:44 +0800

    用户模块完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -31,4 +31,32 @@ $(function(){
 	}
 	render();
 
+	// 点击启用或者禁用按钮弹出模态框
+	//这里的按钮都是动态渲染出来的所以需要注册委托时间
+	$("tbody").on("click",".btn",function(){
+		$("#userModal").modal("show");
+		var id =$(this).parent().data("id");
+		var isDelete=$(this).parent().data("isDelete");
+		isDelete=isDelete===1?0:1;
+
+		$(".btn_confirm").off().on("click",function(){
+ 		$.ajax({
+ 			type:"post",
+ 			url:"/user/updateUser",
+ 			data:{
+ 				id:id,
+ 				isDelete:isDelete
+ 			},
+ 			success:function(data){
+ 				if (data.success) {
+ 					$("#userModal").modal("hide");
+ 					render();
+ 				}
+ 			}
+ 		})
+	})
+
+	});
+	
+
 })
